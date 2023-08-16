3b415b1f2a68c690cbd112e4b7f93fccbe2a4bee
Chmele <killerbbb123@gmail.com>
2023-08-16 16:00:13 +0300

    setup.py changes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'objectutils',         
   packages = ['objectutils'], 
-  version = '0.1.3',    
+  version = '0.1.4',    
   license='MIT',      
   description = 'Utils that extend default dict|list operations', 
   long_description = '''
@@ -23,7 +23,7 @@ Inspired by:
 - [jq](https://jqlang.github.io/jq/)
 ''',
   author = 'Chmele',              
-  url = 'https://github.com/Chmele/difflib/tree/publish',  
+  url = 'https://github.com/Chmele/difflib/tree/main',  
   keywords = ['dict', 'json', 'jq', 'jmespath'], 
   classifiers=[
     'Development Status :: 3 - Alpha',    
