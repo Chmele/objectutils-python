3836eeacb8a70d4dab2253c515d3e74ac3eadb34
Chmele <killerbbb123@gmail.com>
2023-08-17 00:49:12 +0300

    Provide support for lists and dicts in wrapper

--- objectutils/class_wrapper.py
+++ objectutils/class_wrapper.py
@@ -1,8 +1,28 @@
 from .single_dispatch import deep_traverse
-from collections import UserDict
+from collections import UserDict, UserList
+from functools import singledispatch
 
+@singledispatch
+def T(obj):
+    pass
 
-class T(UserDict):
+@T.register(dict)
+def _(obj):
+    return TraverseDict(obj)
+
+@T.register(list)
+def _(obj):
+    return TraverseList(obj)
+
+class TraverseDict(UserDict):
+    def __getitem__(self, key):
+        try:
+            return deep_traverse(self.data, key)
+        except TypeError:
+            return super().__getitem__(key)
+        
+
+class TraverseList(UserList):
     def __getitem__(self, key):
         try:
             return deep_traverse(self.data, key)
