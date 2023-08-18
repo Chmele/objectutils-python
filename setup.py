033271fc753084b83ba204121ca30c4d9766abd8
Chmele <killerbbb123@gmail.com>
2023-08-18 18:56:19 +0300

    fix version number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as file:
 setup(
   name = 'objectutils',         
   packages = ['objectutils'], 
-  version = '0.1.6',    
+  version = '0.2.0',    
   license='MIT',      
   description = 'Utils that extend default dict|list operations', 
   long_description = ld,
