f1084eb9d5edeb3d68fa6393d217b68d4b858fa3
Chmele <killerbbb123@gmail.com>
2023-08-18 18:38:04 +0300

    fix broken import

--- objectutils/__init__.py
+++ objectutils/__init__.py
@@ -1,12 +1,13 @@
-from .main import recursion_level, flatten, per_itempair
-from .single_dispatch import deep_traverse
+from .main import recursion_level, flatten, zip_dicts
+from .single_dispatch import deep_traverse, PathGroup
 from .class_wrapper import T
 
 
 __all__ = [
     "recursion_level",
     "flatten",
-    "per_itempair",
+    "zip_dicts",
     "deep_traverse",
     "T",
+    "PathGroup"
 ]
