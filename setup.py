5abb72520e287245d3501952dcf97917a1f76b05
Vsevolod Hermaniuk <53478956+Chmele@users.noreply.github.com>
2025-03-24 20:40:46 +0200

    fix setup config

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as file:
 setup(
   name = 'objectutils',         
   packages = ['objectutils'], 
-  version = '0.2.0',    
+  version = '0.3.0',    
   license='MIT',      
   description = 'Utils that extend default dict|list operations', 
   long_description = ld,
