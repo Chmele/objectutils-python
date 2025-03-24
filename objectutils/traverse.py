3476ce1054bb991c55f5bc64ac3369338c6b25b9
Vsevolod Hermaniuk <53478956+Chmele@users.noreply.github.com>
2025-03-24 20:40:45 +0200

    cleanup

--- objectutils/traverse.py
+++ objectutils/traverse.py
@@ -1,6 +1,19 @@
 from functools import singledispatch
 from types import FunctionType, BuiltinFunctionType
-from .fallbacks import fallback
+
+
+def fallback(mapping=None):
+    mapping = mapping or {}
+    def dec(func):
+        def wrapper(*args, exception=None, **kwargs):
+            if exception == None:
+                return func(*args, **kwargs)
+            try:
+                return mapping[type(exception)](*args, **kwargs)
+            except Exception as e:
+                raise exception
+        return wrapper 
+    return dec
 
 
 @singledispatch
