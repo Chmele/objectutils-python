9bc1fdc8e2961964215dda59c3b688c3d6c25a8f
Vsevolod Hermaniuk <53478956+Chmele@users.noreply.github.com>
2025-03-24 20:39:34 +0200

    fix version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'objectutils',         
   packages = ['objectutils'], 
-  version = '0.1',    
+  version = '0.1.3',    
   license='MIT',      
   description = 'Utils that extend default dict|list operations', 
   long_description = '''
